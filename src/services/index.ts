0a87266b7ee41784abbac16aae777f69ffc66789
Oladipo Oduola <OduoAb@dipco.local>
2024-10-21 16:23:04 +0100

    Create & Populate Air Quality DB

---
+++ src/services/index.ts
@@ -0,0 +1,51 @@
+import { Pm25Data, AirQualityDB } from "../models";
+
+export let airQualityDB: AirQualityDB = new Map([
+  [
+    1,
+    {
+      Latitude: -54.994998931884766,
+      Longitude: -71.29499816894531,
+      Year: 2000,
+      PM25Level: 7.199999809265137,
+    },
+  ],
+  [
+    2,
+    {
+      Latitude: -54.994998931884766,
+      Longitude: -71.28500366210938,
+      Year: 2000,
+      PM25Level: 7.199999809265137,
+    },
+  ],
+  [
+    3,
+    {
+      Latitude: -54.994998931884766,
+      Longitude: -71.2750015258789,
+      Year: 2000,
+      PM25Level: 7.300000190734863,
+    },
+  ],
+  [
+    4,
+    {
+      Latitude: -54.994998931884766,
+      Longitude: -71.26499938964844,
+      Year: 2000,
+      PM25Level: 7.300000190734863,
+    },
+  ],
+  [
+    5,
+    {
+      Latitude: -54.994998931884766,
+      Longitude: -71.25499725341797,
+      Year: 2000,
+      PM25Level: 7.300000190734863,
+    },
+  ],
+]);
+
+export const retrieveDB = (): AirQualityDB => airQualityDB;
